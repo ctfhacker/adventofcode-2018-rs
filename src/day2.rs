cbfbf5a77b4f44d2f7f276a0c39a9d56c664f05e
thebarbershopper <cld251@gmail.com>
2018-12-06 09:55:06 -0600

    Update with bench

--- src/day2.rs
+++ src/day2.rs
@@ -59,3 +59,15 @@ mod tests {
         assert_eq!(part2("abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz"), "fgij");
     }
 }
+
+/*
+Day2 - Part1/(default)  time:   [292.68 us 294.12 us 295.69 us]                                   
+Found 5 outliers among 100 measurements (5.00%)
+  4 (4.00%) high mild
+  1 (1.00%) high severe
+
+Day2 - Part2/(default)  time:   [3.6265 ms 3.6415 ms 3.6588 ms]                                    
+Found 2 outliers among 100 measurements (2.00%)
+  1 (1.00%) high mild
+  1 (1.00%) high severe
+*/
