3562a4531772e331dc47816b6213168bad150309
thebarbershopper <cld251@gmail.com>
2018-12-11 15:46:51 -0600

    add lib

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,13 @@ extern crate aoc_runner_derive;
 
 #[macro_use] 
 extern crate itertools;
+#[macro_use] 
+extern crate lazy_static;
+extern crate regex;
 
 pub mod day1;
 pub mod day2;
+pub mod day3;
+pub mod day4;
 
 aoc_lib!{ year = 2018 }
